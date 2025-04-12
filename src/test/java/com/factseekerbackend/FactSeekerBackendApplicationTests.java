5ae6be70e5f1f83a621490626f9d65eb1c80a437
enohs <kmsn01@naver.com>
2025-04-12 16:04:53 +0900

    Add Spring Boot initial project

---
+++ src/test/java/com/factseekerbackend/FactSeekerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factseekerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactSeekerBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
