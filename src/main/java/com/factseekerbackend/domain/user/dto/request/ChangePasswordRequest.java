abc436bd0c2a0ddc06c0ad5cd1ef894042ad750b
enohs <kmsn01@naver.com>
2025-08-02 18:21:16 +0900

    refactor: 어노테이션 추가

--- src/main/java/com/factseekerbackend/domain/user/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/factseekerbackend/domain/user/dto/request/ChangePasswordRequest.java
@@ -1,8 +1,12 @@
 package com.factseekerbackend.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangePasswordRequest {
   private String oldPassword;
   private String newPassword;
--- src/main/java/com/factseekerbackend/domain/user/dto/request/UserRegisterRequest.java
+++ src/main/java/com/factseekerbackend/domain/user/dto/request/UserRegisterRequest.java
@@ -1,8 +1,12 @@
 package com.factseekerbackend.domain.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRegisterRequest {
 
   private String loginId;
