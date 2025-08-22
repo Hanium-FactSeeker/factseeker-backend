2122192741d12200f110f36e0a9609aaa5db746b
김시온 <kmsn01@naver.com>
2025-08-22 19:18:21 +0900

    Revert "fix: 서버 url 변경했습니다"

--- src/main/java/com/factseekerbackend/global/politicalTranding/service/TrendService.java
+++ src/main/java/com/factseekerbackend/global/politicalTranding/service/TrendService.java
@@ -1,29 +1,29 @@
-package com.factseekerbackend.global.politicalTranding.service;
-
-import com.factseekerbackend.global.exception.BusinessException;
-import com.factseekerbackend.global.exception.ErrorCode;
-import java.util.ArrayList;
-import java.util.List;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-@Slf4j
-@Service
-public class TrendService {
-
-  private volatile List<String> latestTrends = new ArrayList<>();
-
-  public List<String> getLatestTrends() {
-    if (CollectionUtils.isEmpty(latestTrends)) {
-      throw new BusinessException(ErrorCode.TRENDS_NOT_AVAILABLE);
-    }
-    return latestTrends;
-  }
-
-  public void updateTrends(List<String> newTrends) {
-    this.latestTrends = newTrends;
-    log.info("Updated Trends: {}", newTrends);
-  }
-
-}
+//package com.factseekerbackend.global.politicalTranding.service;
+//
+//import com.factseekerbackend.global.exception.BusinessException;
+//import com.factseekerbackend.global.exception.ErrorCode;
+//import java.util.ArrayList;
+//import java.util.List;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//import org.springframework.util.CollectionUtils;
+//
+//@Slf4j
+//@Service
+//public class TrendService {
+//
+//  private volatile List<String> latestTrends = new ArrayList<>();
+//
+//  public List<String> getLatestTrends() {
+//    if (CollectionUtils.isEmpty(latestTrends)) {
+//      throw new BusinessException(ErrorCode.TRENDS_NOT_AVAILABLE);
+//    }
+//    return latestTrends;
+//  }
+//
+//  public void updateTrends(List<String> newTrends) {
+//    this.latestTrends = newTrends;
+//    log.info("Updated Trends: {}", newTrends);
+//  }
+//
+//}
