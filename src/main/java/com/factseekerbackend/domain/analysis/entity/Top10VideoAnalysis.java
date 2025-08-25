7af2ca80b3559eda03726cdb44b256a6acfe032d
BBang <136908616+baaamk@users.noreply.github.com>
2025-08-26 03:02:23 +0900

    Revert "Revert "Revert "[Merge] 개발 환경에서 배포 환경으로의 merge"""

--- src/main/java/com/factseekerbackend/domain/analysis/entity/Top10VideoAnalysis.java
+++ src/main/java/com/factseekerbackend/domain/analysis/entity/Top10VideoAnalysis.java
@@ -1,28 +1,29 @@
-package com.factseekerbackend.domain.analysis.entity.video;
+package com.factseekerbackend.domain.analysis.entity;
 
-import com.factseekerbackend.domain.analysis.entity.AnalysisStatus;
-import com.factseekerbackend.domain.analysis.service.fastapi.FactCheckResultService;
-import com.factseekerbackend.domain.history.entity.AnalysisHistory;
-import com.factseekerbackend.domain.user.entity.User;
-import jakarta.persistence.*;
+import com.factseekerbackend.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
-
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder(toBuilder = true)
 @Getter
-public class VideoAnalysis {
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Top10VideoAnalysis {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "video_analysis_id")
-    private Long id;
-
-    @Column(name = "video_id", length = 225, nullable = false)
+    @Column(name = "video_id", length = 32, nullable = false)
     private String videoId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", length = 20)
+    private VideoAnalysisStatus status;
 
     @Column(name = "video_url", length = 255)
     private String videoUrl;
@@ -50,12 +51,5 @@ public class VideoAnalysis {
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", length = 50)
-    private AnalysisStatus status;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
 }
+
