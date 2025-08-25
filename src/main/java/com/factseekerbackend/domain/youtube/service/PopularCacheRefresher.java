e59489ce7f5ae5c338e4d35bc342eb1e29750e83
baaamk <brandon55321@gmail.com>
2025-08-25 15:24:54 +0900

    refactor: 스케쥴러 수정
    스케쥴러 수정했습니다.

--- src/main/java/com/factseekerbackend/domain/youtube/service/PopularCacheRefresher.java
+++ src/main/java/com/factseekerbackend/domain/youtube/service/PopularCacheRefresher.java
@@ -12,7 +12,7 @@ public class PopularCacheRefresher {
 
     private final PopularPoliticsService popularPoliticsService;
 
-    @Scheduled(cron = "0 7 * * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 * * * *", zone = "Asia/Seoul")
     public void refreshPopularPolitics() throws IOException {
         // Top10을 규칙적으로 갱신 (Redis 해시 + 동일 timestamp)
         popularPoliticsService.refreshTopN(10);
