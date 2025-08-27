9718e5235e1d600a6f6082c265eb5a79eab8b604
Jung suk woo <jungsw1106@mju.ac.kr>
2025-08-27 22:01:37 +0900

    Revert "Merge pull request #117 from Hanium-FactSeeker/develop"
    This reverts commit 8c7d7e3bafd0dbcd97c16cafa70cf8349c09f638, reversing
    changes made to 6a8e84cf10476ab0e829142bee368b9880950fe7.

--- src/main/java/com/factseekerbackend/domain/apify/dto/request/ApifyRequest.java
+++
@@ -1,7 +0,0 @@
-package com.factseekerbackend.domain.apify.dto.request;
-
-public record ApifyRequest(
-        String username,      // SNS 계정
-        String full_text,     // 게시글 본문
-        String date           // 게시일자
-) {}
