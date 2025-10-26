e5a29cf3f597ccbc0f3bb3b672f81c67d4bb6eba
baaamk <brandon55321@gmail.com>
2025-10-27 01:46:32 +0900

    fix: 유튜브 화이트 리스트 수정

--- src/main/java/com/factseekerbackend/domain/youtube/config/WhiteListChannels.java
+++ src/main/java/com/factseekerbackend/domain/youtube/config/WhiteListChannels.java
@@ -7,54 +7,21 @@ public class WhiteListChannels {
      * 화이트리스트 채널 ID (공영방송·언론사 제외, Playboard Top100 기준)
      */
     public static final List<String> WHITE_LIST_CHANNELS = List.of(
-            "UCGLgwNqHi_smqbM8K95kB6A", // 편들어주는 파생 방송
-            "UCrxe-Msh2j6ICbfUaDJj81g", // 이슈카톡
-            "UCc6sEta0lPmVJ3TFIw8s-wA", // 민주픽
-            "UC-qaxvBfg9TYYkAbKNviMBg", // 트렌드쇼츠
-            "UCzuBleEdJox6p_p5wexcp5A", // 정치일주
-            "UCs3ylOvYPllwAWHRAfBIlnA", // 일구팔구 1989
-            "UCgeOlLcX6PReHdWImEnUVTg", // 스픽스
-            "UC6kOh6PXbc75dYTDSdbkYQA", // 민주카톡
-            "UCuV_etBxr697iLA_lFByyfA", // 까발려TV
-            "UC_qkrKk1QpIjdd9t_Ku8TZA", // 명명백백
-            "UCq-4J_OULaSy3QsEG05FaYg", // 정.길.희 정치의길은희망
-            "UCsKB8l_b1gUVWwqk8DoQyWA", // 일류
-            "UCMvbvHHnMC9DNzOymwsPTMQ", // 폴리티카 POLITICA
-            "UCMYhq9OyGI5UEz_NTAoHY7A", // [팟빵] 매불쇼
-            "UCuka4cOMWar3MM0WDtE9ehw", // 애국청년김태풍
-            "UCtZsqQeC5tWHAKxKxmGBvxQ", // 투모로
-            "UC_iT-dQV96rPviS4G4p5b9A", // 전한길뉴스 1waynews
-            "UCAAvO0ehWox1bbym3rXKBZw", // 김어준의 겸손은힘들다 뉴스공장
-            "UCOqCunaF9qVN8bXwsK0HT3g", // 펜앤마이크TV
-            "UCHDYKIRxMEItLVoqtmGq3aA", // 정치창
-            "UC-SlAphpxXKfbqxN8nS-68w", // 너알아TV 대국민방송
-            "UCvK7qaD2xgV7-egDdtKBbbQ", // 옳소TV
-            "UCLaZK1q3f2OPv93TyAcTbSw", // 숏쇼 ShortsShow
-            "UCfdwCAK9QHC7XMyKbgmqnjg", // 한두자니
             "UCFWIofVBqseX6Bmd9Dn7kgg", // 성창경TV
-            "UCK6IkFBFPRo0QOUgUJ9VETA", // 주관적정치
-            "UCTTmtS2ljy1vyl_s-d_LEHQ", // MBC 라디오 시사
-            "UCEEKWn9nLv76kJ_e5okT1IQ", // 나의최욱
-            "UC7oX2slW1INQdwLX_eC0BKg", // 리업윤통
-            "UCvRCrAL6zDiPBriGuK73oRw", // 1분뉴스쇼
-            "UCl_YDj8DFZSqESwDmmsHrqw", // 어준이뉴스
-            "UCbouR-45rChK6ou95zSlNxA", // 사이다 이슈-시사뉴스
-            "UCsVxyWLL96SSeoNmeCvl6Cw", // 정알못 NEWS
-            "UCd6tMX5cbTf6HNf1iug0HrQ", // 이마까TV
-            "UC8tJM0XF53xTSWXeMLCUoeg", // 세상경사
-            "UC66eY9QAil504XyknoBnugg", // 국민주권
-            "UCw3y9TulIeo3WwppodqgcCw", // 정치파란
-            "UCGUeucMBBJ5XYCI7xGu1D7g", // 냥냥사랑
-            "UC07sfpMTLmMWBhbMglEv05w", // 정치브리프
-            "UCN0qXG4fM6kOfrEBrsGUXeA", // 보통의 상식과 공정
-            "UCljnbFCt-4doBr7wtEIIbbw", // 김용민TV
-            "UC1bhliwogClGAd73JYyUfLw", // 이슈로그
-            "UCd55cOYcz2CaC2MbiqHe9Hw", // 시사잇슈
-            "UC00UtUHoVNBhZh5-mQouNEg", // 요즘 정치 이슈
-            "UC41Ic2Z_XniljS60WCuQ0sg", // 솔직한여자TV
-            "UCmEx0Z66HZdOrYqQhM46w3A", // 단골이슈
+            "UCMYhq9OyGI5UEz_NTAoHY7A", // [팟빵] 매불쇼
             "UCSd6jlFPB6iVG3eCTH7XJZA", // 시사포커스TV
-            "UCzK0_j7GR2wKyNDMpoy3VXA", // 지금 국회는
-            "UC8SZ88GQa9XWwZxLNibO3ug"  // 배승희 변호사
+            "UChgN9s3Y3R0FBn1yB-YVmVA", // 정치핫소스
+            "UCOqCunaF9qVN8bXwsK0HT3g", // 펜앤마이크TV
+            "UCrxe-Msh2j6ICbfUaDJj81g", // 이슈카톡
+            "UCgeOlLcX6PReHdWImEnUVTg", // 스픽스
+            "UCNzWZmsJ2QmBss30LeZZTdg", // 서정욱TV
+            "UC8SZ88GQa9XWwZxLNibO3ug", // 배승희 변호사
+            "UCuV_etBxr697iLA_lFByyfA", // 까발려TV
+            "UCAAvO0ehWox1bbym3rXKBZw", // 김어준의 겸손은힘들다 뉴스공장
+            "UCOUFJNPhk0P1ICV5nC1yyHQ", // 더민숏전시관
+            "UCc6sEta0lPmVJ3TFIw8s-wA", // 민주픽
+            "UCXizVvs2c7LtFZAu25sK5Fg", // 정치파랑새
+            "UCHDYKIRxMEItLVoqtmGq3aA"  // 정치창
     );
+
 }
